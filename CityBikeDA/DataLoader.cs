25e6d45aded6a901f803f31d8f65be034e39b7cd
Kacper Frączek <kacper@tee.pl>
2026-02-10 14:50:41 +0100

    add DataLoader helper static class

--- CityBikeDA/DataLoader.cs
+++ CityBikeDA/DataLoader.cs
@@ -1,6 +1,38 @@
+using System.Net.Http.Headers;
+
 namespace CityBikeDA;
 
-public class DataLoader
+public static class DataLoader
 {
+    public static List<BikeTrip> Load(string path)
+    {
+        var trips = new List<BikeTrip>();
+        if (!File.Exists(path))
+        {
+            Console.WriteLine($"File {path} does not exist");
+            return trips;
+        }
+
+        using (StreamReader reader = new StreamReader(path))
+        {
+            reader.ReadLine();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(line)) continue;
+
+                var values = line.Split(',');
+
+                if (values.Length >= 13)
+                {
+                    var trip = new BikeTrip(values);
+
+                    trips.Add(trip);
+                }
+            }
+            return trips;
+        }
+    }
     
 }
