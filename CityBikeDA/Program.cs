351c85a2fbbe24276f87e384564e9e040016b93f
Kacper Frączek <kacper@tee.pl>
2026-02-15 23:20:39 +0200

    main to print results and pass csv files

--- CityBikeDA/Program.cs
+++ CityBikeDA/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿namespace CityBikeDA;
+
+
+class Program
+{
+    static void Main()
+    {
+        var trips = DataLoader.Load("CityBikeFeb2020.csv");
+        var weather = WeatherLoader.Load("NYC_weather.csv");
+        var febWeather = weather
+            .Where(w => w.Time.Year == 2020 && w.Time.Month == 2)
+            .ToList();
+        var longest = Analyze.LongestRide(trips);
+        var shortest = Analyze.ShortestRide(trips);
+        Console.WriteLine($"Longest: {(longest.EndedAt - longest.StartedAt).TotalMinutes:F0} min\n");
+        Console.WriteLine($"Shortest: {(shortest.EndedAt - shortest.StartedAt).TotalMinutes:F0} min\n");
+        
+        Analyze.AnalyzePeakHoursByDayType(trips);
+        Analyze.CalculateStationBalance(trips);
+        Analyze.PrintTripDurations(trips);
+        
+        Analyze.PrintMostPopularRoute(trips);
+        BikeWeatherAnalysis.RideDurationByWeather(trips, febWeather);
+        BikeWeatherAnalysis.RidesByTemperature(trips,febWeather);
+    }
+}
